c413a21ebcf7568e15599cf049c1c9ce52190a7b
prrogramistro <r.grigorev1502@gmail.com>
2025-03-24 23:32:26 +0300

    add cors

--- BinanceApi.Web/Program.cs
+++ BinanceApi.Web/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod());
+
 app.UseStaticFiles();
 
 app.UseHttpsRedirection();
